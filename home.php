115aeff15d0323f8b85c42f8424b8bfc21936e16
eddiegemayel <eddiegem16@gmail.com>
2015-06-10 17:33:09 -0400

    Logout now works
    session destroyed , also set username to empty as back up just in case.
    fix #11

--- home.php
+++ home.php
@@ -90,7 +90,7 @@ echo '<!DOCTYPE html>
 				';
 ?>  
 		</div><!-- end of content div-->
-		</div>
+		</div><!-- end of wrapper div -->
 	</body>
 	<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
 	<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
--- logout.php
+++ logout.php
@@ -1,8 +1,14 @@
 <?php
+	//retrieve session
 	session_start();
 
+	//end it
 	session_destroy();
 
+	//just in case
+	$_SESSION['username'] = '';
 	// var_dump($_SESSION['username']);
+
+	//put them back to login screen
 	header("Location: index.html");
 ?>
